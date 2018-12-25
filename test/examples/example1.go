572f4085466c76e8da1208c3a3ec044dedd10a19
root <root@ubuntu.ubuntu-domain>
2018-12-25 21:31:14 +0800

    2 commit

---
+++ test/examples/example1.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+var x, y = 1, 10
+func main() {
+    var b bool = true
+    var c int = 0
+    d, f, e := c, 12, "123"
+    fmt.Println("Hello World...!", b, c, d, f, e, x, y)
+}
